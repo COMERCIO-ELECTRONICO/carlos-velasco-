b15a19f735c46ee6b6151012eb7bf31aa7e800b4
carlos velasco <velascoalex121@gmail.com>
2020-05-13 14:26:20 -0500

    deber

--- deber3.js
+++ deber3.js
@@ -1,72 +1,69 @@
+var arregloJsons = [{
+    nombre: 'carlos',
+    edad: 21,
+    fechaNacimiento: new Date(),
+    telf: 091883847373
+}]
 
+var usuarioNuevo = {
+    nombre: 'jose',
+    edad: 30,
+    fechaNacimiento: new Date(),
+    telf: 55555555545
+}
 
- var nombreUsuario={nombre:'carlos',edad:21,fechaNaci: new Date(1998,09,12),telefono:"88888885841"}
- function usuarioExiste(arreglo,nombreUsuario,error,insertarUsuario){
-     for (var index = 0; index < arreglo.length; index++) {
-         if(arreglo[index].nombre===nombreUsuario.nombre){
-             return error()
-         }    
-     }
-     insertarUsuario(arreglo,nombreUsuario) 
- }
- function error(){
-     console.error("El usuario ya existe")
- }
- function insertarUsuario(arreglo,nombreUsuario){
- arreglo.push(nombreUsuario)
- console.log(arreglo)
- console.info("El usuario se creo correctamente")
- }
- usuarioExiste(arreglo,nombreUsuario,error,insertarUsuario)
+function crearUsuario(arreglo, usuarioACrear, cb) {
+    var usuarioEncontrado = arreglo
+        .find(
+            (item) => {
+                return item.nombre === usuarioACrear.nombre
+            })
+    // console.log(usuarioEncontrado);
+    if (usuarioEncontrado !== undefined) {
+        cb({
+            mensaje: 'Usuario creado',
+            usuario: usuarioACrear,
+            arreglo
+        })
+    } else {
+        arreglo.push(usuarioACrear)
 
- var nombreUsuario={nombre:'carlos',edad:21,fechaNaci: new Date(1998,09,12),telefono:"88888885841"}
- var nuevosDatos={nombre:'juan',edad:21,fechaNaci: new Date(1999,02,04),telefono:"012345678765"}
- function consulta(arreglo,nombreUsuario,nuevosDatos,error,editar){
-     for (var index = 0; index < arreglo.length; index++) {
-         if(arreglo[index].nombre===nombreUsuario.nombre){
-             return editar(arreglo,index,nuevosDatos)
-         }    
-     }
-    error()
- }
- function error(){
-     console.error("El usuario no existe")
- }
- function editar(arreglo,index,nuevosDatos){
-     arreglo[index]=nuevosDatos
-     console.log(arreglo)
-     console.log("Datos editados")
- }
- consulta(arreglo,nombreUsuario,nuevosDatos,error,editar)
+        cb({
+            mensaje: 'Usuario existe',
+            usuario: usuarioACrear,
+            arreglo
+        })
+    }
+}
 
- function listar(arreglo,imprimir){
-     for (let index = 0; index < arreglo.length; index++) {
-        imprimir(arreglo[index].nombre)
-     }
- }
- function imprimir(nombre){
-     console.log(nombre)
- }
- listar(arreglo,imprimir)
+crearUsuario(arregloJsons, usuarioNuevo, (respuestaCallback) => {
+    console.log(`mensaje: ${respuestaCallback.mensaje}, usuario: ${respuestaCallback.usuario}`)
+    console.log(respuestaCallback.arreglo)
+})
 
-/************************************************************************************************************* */
-var nombreUsuario={nombre:'carlos',edad:21,fechaNaci: new Date(1998,09,12),telefono:"88888885841"}
- function consulta(arreglo,nombreUsuario,error,eliminar){
-     for (var index = 0; index < arreglo.length; index++) {
-         if(arreglo[index].nombre===nombreUsuario.nombre){
-             return eliminar(arreglo,index,)
-         }    
-     }
-    error()
- }
- function error(){
-     console.error("El usuario no existe")
- }
- function eliminar(arreglo,index){
-  for (let i = index; i < arreglo.length-1; i++) {
-      arreglo[i]=arreglo[i+1]
-  }
-     console.log(arreglo)
-     console.log("Datos eliminados")
- }
- consulta(arreglo,nombreUsuario,error,eliminar)
+function eliminarUsuario(arreglo, nombreUsuario, cb) {
+
+    var indiceUsuario = arreglo
+        .findIndex(
+            (item) => {
+                return item.nombre === nombreUsuario
+            })
+    console.log(indiceUsuario);
+    if (indiceUsuario !== -1) {
+        arreglo.splice(indiceUsuario, 1)
+        cb({
+            mensaje: 'eliminado'
+        })
+    } else {
+        cb({
+            mensaje: 'eliminado no'
+        })
+    }
+
+}
+
+eliminarUsuario(arregloJsons, 'carlos', (resultadoCallback) => {
+    console.log(resultadoCallback)
+    console.log(arregloJsons)
+})
+console.log(arregloJsons)
